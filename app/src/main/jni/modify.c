c2909adad8fb32c1ae0f16a4e4ff7e6e2963209a
Po1lux <sean_chen31@163.com>
2019-12-04 11:40:38 +0800

    -

--- app/src/main/jni/modify.c
+++ app/src/main/jni/modify.c
@@ -11,22 +11,105 @@
 #define TAG "cs"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__)
 
+/**
+ * 找到DEX的文件魔术"dex\n035"
+ * @param searchStartPage
+ * @return
+ */
 int findMagic(void* searchStartPage){
     char magic[10];
-//    memcpy(magic,"dex\n035",8);
-    memcpy(magic,"oat\n",5);
+    memcpy(magic,"dex\n035",8);
     if(memcmp(searchStartPage,magic,5)){
         return 0;
     }else{
         return 1;
     }
 }
+/**
+ * leb128格式数据转成int格式数据
+ * @param addr
+ * @param count
+ * @return
+ */
+int readUleb128(int *addr, int count){
+    int *address;
+    int result;
+    signed int bytes;
+    signed int value_1;
+    signed int value_2;
+    signed int value_3;
+
+    address = addr;
+    result = *(char *)addr;
+    bytes = 1;
+    if ( (unsigned int)result > 0x7F )//判断最高bit 0x7f = 0111 1111
+    {
+        value_1 = *((char *)address + 1);//取下一个8bit
+        //第一个字节与第二个字节去除最高位，拼成一个2字节数值
+        result = result & 0x7F | ((value_1 & 0x7F) << 7);
+        bytes = 2;
+        if ( value_1 > 127 )//如果第二个字节最高位为1，即后面还有数值
+        {
+            value_2 = *((char *)address + 2);//取第三个字节
+            result |= (value_2 & 0x7F) << 14;//移位，拼成3字节数值
+            bytes = 3;
+            if ( value_2 > 127 )//如果第三个字节最高位为1，接着取下面的数值
+            {
+                value_3 = *((char *)address + 3);
+                bytes = 4;
+                result |= (value_3 & 0x7F) << 21;
+                if ( value_3 > 127 )//如果第四个字节最高位为1，接着取下面的数值
+                {
+                    bytes = 5;
+                    result |= *((char *)address + 4) << 28;
+                }//结束判断，此时可以取得最大值为：
+                // 1111111 1111111 1111111 1111111 1111111 = 7ffffffffh，基本满足需要
+            }
+        }
+    }
+    *(int *)count = bytes;//将leb128数值的字节数返回给count指针
+    return result;//返回转换成的int数值
+}
+
+int getStrIdx(int searchStartPos,char *str,int size){
+    int index;
+    int string_ids;
+    int string_data_off;
+    int *stringAddr;
+    int string_num_mutf8;
+
+    if(*(int *)(searchStartPos + 56))//string_ids_size
+    {
+        index = 0;
+        string_ids = searchStartPos + *(int *)(searchStartPos+60);
+        while(1) {
+            string_data_off = *(int *)string_ids;
+            string_ids +=4;
+            stringAddr = (int *)(searchStartPos + string_data_off);
+            string_num_mutf8 = 0;
+            if( readUleb128(stringAddr,(int)&string_num_mutf8) == size
+                && !strncmp((char *)stringAddr + string_num_mutf8,str,size)){
+                LOGD("(int)&string_num_mutf8:%d",(int)&string_num_mutf8);
+                LOGD("(int)&string_num_mutf8:%d",string_num_mutf8);
+                break;
+            }
+            ++index;
+            if(*(int *)(searchStartPos+56) <= index ) {
+                index = -1;
+                break;
+            }
+        }
+    }else {
+        index = -1;
+    }
+    return index;
+}
+
 
 JNIEXPORT jint JNICALL Java_cn_pollux_modifydalvikbytecode_MainActivity_modifyBytecode
         (JNIEnv *env, jclass clazz) {
-    /**
-     * 寻找dex的地址
-     */
+
+    //寻找dex的地址
     int pid = getpid();
     void *start;
     void *end;
@@ -39,8 +122,7 @@ JNIEXPORT jint JNICALL Java_cn_pollux_modifydalvikbytecode_MainActivity_modifyBy
         while (fgets(line, sizeof(line), fp) != NULL) {
             //data/dalvik-cache/arm/data@app@cn.pollux.modifydalvikbytecode-2@base.apk@classes.dex
             //data/dalvik-cache/data@app@cn.pollux.modifydalvikbytecode-2.apk@classes.dex
-            if (strstr(line, "@cn.pollux.modifydalvikbytecode") !=
-                NULL) {
+            if (strstr(line, "@cn.pollux.modifydalvikbytecode") !=NULL) {
                 if (strstr(line, "classes.dex") != NULL) {
                     LOGD("line:%s",line);
                     char *s = strchr(line, '-');
@@ -64,14 +146,16 @@ JNIEXPORT jint JNICALL Java_cn_pollux_modifydalvikbytecode_MainActivity_modifyBy
     unsigned int searchStartPage = startAddr;
     unsigned int searchStartPos = 0;
 
+    //从开始地址搜索，去掉40h的odex头
     while(!findMagic((void*)(searchStartPage+40))) {
         searchStartPage += pageSize;
     }
     searchStartPos = searchStartPage+40;
     LOGD("searchStartPage = %x", searchStartPage);
     LOGD("searchStartPos = %x", searchStartPos);
-//    char* con = (char*)malloc(20);
-//    memcpy(con,(void*)searchStartPos,19);
-//    LOGD("magic:%s",con);
+
+    int classStrIdx = 0;
+    classStrIdx = getStrIdx(searchStartPos,"Lcn/pollux/modifydalvikbytecode/Add;",strlen("Lcn/pollux/modifydalvikbytecode/Add;"));
+    LOGD("classStrIdx:%x",classStrIdx);
     return 1;
 }
